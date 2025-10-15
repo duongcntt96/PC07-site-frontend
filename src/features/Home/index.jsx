0f805b600446a2e2859736dc752672e62c1cbfe1
Dương <78836818+duongcntt96@users.noreply.github.com>
2025-10-15 15:32:20 +0700

    Update index.jsx

--- src/features/Home/index.jsx
+++ src/features/Home/index.jsx
@@ -55,7 +55,11 @@ function Home() {
         <div className="avatar">
           <img src={avatar} alt="" />
           <div className="circle">
-            <div className="circle-box"></div>
+            <div className="circle-box">
+
+              <iframe width="560" height="315" src="https://www.youtube.com/embed/QocEbM-IhN4?si=_jE7_TMiaORTkWTu" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+              
+            </div>
           </div>
         </div>
       </div>
