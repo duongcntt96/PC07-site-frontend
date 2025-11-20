c2c24005f97c0132b8384b8c57245e61b2ac746c
Dương <78836818+duongcntt96@users.noreply.github.com>
2025-11-20 17:02:58 +0700

    Update index.jsx

--- src/features/Home/index.jsx
+++ src/features/Home/index.jsx
@@ -61,7 +61,7 @@ function Home() {
       </div> */}
       <div>
         <h4>Robot chữa cháy và cứu nạn, cứu hộ</h4>
-        <iframe width="100%" height="100%" src="https://www.youtube.com/embed/QocEbM-IhN4?si=_jE7_TMiaORTkWTu" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+        <iframe style="width: 100%; max-width: 1120px; height: 560px; border: none;" src="https://www.youtube.com/embed/QocEbM-IhN4?si=_jE7_TMiaORTkWTu" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
       </div>
 
       <div>
